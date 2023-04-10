63d0a258c980f78f366f878a31b91bff2c893063
oscarsangpa <oscarsangrador@gmail.com>
2023-04-10 19:05:05 +0200

    feat: create reducer to manage the local state and add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,64 @@
+import { useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { type ReducerState, type ReducerAction } from "./types";
+
+const initialState: ReducerState = {
+  fromLanguage: "auto",
+  toLanguage: "en",
+  fromText: "",
+  result: "",
+  loading: false,
+};
+
+function reducer(state: ReducerState, action: ReducerAction) {
+  const { type } = action;
+
+  if (type === "INTERCHANGE_LANGUAGES") {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage,
+    };
+  }
+
+  if (type === "SET_FROM_LANGUAGE") {
+    return {
+      ...state,
+      fromLanguage: action.payload,
+    };
+  }
+
+  if (type === "SET_TO_LANGUAGE") {
+    return {
+      ...state,
+      toLanguage: action.payload,
+    };
+  }
+
+  if (type === "SET_FROM_TEXT") {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: "",
+    };
+  }
+
+  if (type === "SET_RESULT") {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload,
+    };
+  }
+
+  return state;
+}
 
 function App() {
+  const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] =
+    useReducer(reducer, initialState);
   return (
     <div className="App">
       <h1>Translator</h1>
---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+export interface ReducerState {
+  fromLanguage: string;
+  toLanguage: string;
+  fromText: string;
+  result: string;
+  loading: boolean;
+}
+
+export type ReducerAction =
+  | { type: "INTERCHANGE_LANGUAGES" }
+  | { type: "SET_FROM_LANGUAGE"; payload: string }
+  | { type: "SET_TO_LANGUAGE"; payload: string }
+  | { type: "SET_FROM_TEXT"; payload: string }
+  | { type: "SET_RESULT"; payload: string };
